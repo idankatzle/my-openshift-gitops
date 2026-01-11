d552555b7cdd41512dabe3d882e6a236a31fbb9c
idankatzle <t0506014365@gmail.com>
2026-01-11 12:31:49 +0200

    Testing automation via webhook

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('<h1>Hellow idan katz </h1><p>Node.js is running on OpenShift.</p>'));
+app.get('/', (req, res) => res.send('<h1>Hello from Automated Webhook </h1><p>Node.js is running on OpenShift.</p>'));
 app.listen(8080);
