7fc78e838f89087cc8d38be34d8eee2866305366
idankatzle <t0506014365@gmail.com>
2026-01-11 12:19:04 +0200

    hello

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('<h1>Success!</h1><p>Node.js is running on OpenShift.</p>'));
+app.get('/', (req, res) => res.send('<h1>Hellow idan katz </h1><p>Node.js is running on OpenShift.</p>'));
 app.listen(8080);
