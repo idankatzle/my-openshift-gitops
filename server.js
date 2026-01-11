f905c4aa4c6948bc3fa439e7af5290d39d42a48b
idankatzle <t0506014365@gmail.com>
2026-01-11 17:29:43 +0200

    Add DB connection test

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('<h1>Hello from Automated Webhook </h1><p>Node.js is running on OpenShift.</p>'));
-app.listen(8080);
+const port = 8080;
+
+app.get('/', (req, res) => {
+  const dbHost = process.env.DB_HOST || 'Not Connected';
+  res.send(`<h1>Database Connection Test</h1><p>Connecting to: ${dbHost}</p>`);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
